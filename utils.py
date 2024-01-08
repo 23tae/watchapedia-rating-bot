c58656d967b76debb7b3649326efb332a68e66b3
23tae <taehoonkim.dev@gmail.com>
2024-01-08 11:01:42 +0900

    Refactor: 함수 별도 파일에 분리

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
+rating_classes_filename = './data/rating_classes.txt'
+rating_words_filename = './data/rating_words.txt'
+movie_urls_filename = "./data/movie_urls.txt"
+
 
 def get_target_class_name(rating: str) -> str:
-    class_name_file = open('./data/rating_classes.txt', 'r')
+    class_name_file = open(rating_classes_filename, 'r')
     class_names = class_name_file.readlines()
     converted_rating = int(float(rating) * 2)
     class_name = class_names[converted_rating - 1].rstrip('\n')
@@ -8,9 +12,9 @@ def get_target_class_name(rating: str) -> str:
 
 
 def get_current_class_name(word: str) -> str:
-    with open('./data/rating_classes.txt', 'r') as file:
+    with open(rating_classes_filename, 'r') as file:
         classes = file.readlines()
-    with open('./data/rating_words.txt', 'r') as file:
+    with open(rating_words_filename, 'r') as file:
         words = file.readlines()
     for w in words:
         if word in w:
