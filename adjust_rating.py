4cfbc083ea9e82e2279d0dcab59ecaa45860552d
23tae <taehoonkim.dev@gmail.com>
2024-01-22 13:52:48 +0900

    Chore: 미사용 import문 제거

--- adjust_rating.py
+++ adjust_rating.py
@@ -9,7 +9,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import StaleElementReferenceException, TimeoutException, NoSuchElementException, ElementNotInteractableException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.common.action_chains import ActionChains
 import ssl
 import time
